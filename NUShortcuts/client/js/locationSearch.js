c6e69a3b26d4e368935f0cde944d193f34ae1275
Jiachao Lin <lin_jiachao@hotmail.com>
2017-06-25 19:53:22 +0800

    Fix Location Search Logic
    Location search page works now,
    isvalidLocation logic corrected

--- NUShortcuts/client/js/locationSearch.js
+++ NUShortcuts/client/js/locationSearch.js
@@ -1,50 +1,74 @@
-Template.locationSearch.rendered = function(){
+Template.locationSearch.onRendered(function() {
   $("#locationSearch-link").addClass('selected');
   $("#navigation-link").removeClass('selected');
   $("#settings-link").removeClass('selected');
   $("#popularLocations-link").removeClass('selected');
   $("#map-link").removeClass('selected');
-  //alert("rendered");
-}
+});
 
 Template.locationSearch.events({
-  "submit .locationInput": function(event){
-    var locationToFind = trimInput(event.target.locationSearch.value);
+  "submit .locationSearch": function(event) {
+    var locationToFind = trimInput(event.target.locationToFind.value);
 
-    if(isNotEmpty(locationToFind) &&
-    isValidLocation(locationToFind) ){
+    if (isNotEmpty(locationToFind) &&
+      isValidLocation(locationToFind)) {
+      Bert.alert("Location found", "success", "growl-top-right");
 
-      //TODO: link to the list of locations
-      //TODO: return location info
-      //TODO: return Listof rooms
+      //render the location details
+      var v = finalMap[locationToFind];
+      $('#locationName').text(locationToFind);
+      $('#faculty').text("Faculty: " + v.faculty);
+      //coordinates
+      var coordinates = v.latlongCoordinates;
+      $('#coordinates').text("Latitude: " + coordinates[0])
+      $('#coordinates').append("<br />" + "Longitude: " + coordinates[1]);
+      //roomList
+      $('#roomList').text("List of Rooms: ").append("<br />");
+      for(room in v.roomList){
+        $('#roomList').append("<br />" + v.roomList[room]);
+      }
+      //edges
+      $('#edges').text("Places you can go to from here:").append("<br />");
+      for(edge in v.edges){
+        $('#edges').append("<br />" + edge);
+      }
+
+      //shows the location details layout
+      $(".locationDetails").css('visibility', 'visible');
+
+    } else {
+      //shows the location details layout
+      $(".locationDetails").css('visibility', 'hidden');
     }
 
-    return false;//prevent submitting of form
+    //keeps the input inside the field
+    event.target.locationToFind.value = locationToFind;
+
+    return false; //prevent submitting of form
   }
 });
 
 //Validation Rules
 
 // Trim Helper
-var trimInput = function(val){
-	return val.replace(/^\s*|\s*$/g, "");
+var trimInput = function(val) {
+  return val.replace(/^\s*|\s*$/g, "");
 };
 
-var isNotEmpty = function(value){
-	if (value && value !== ''){
-		return true;
-	}
-	Bert.alert("Please fill in the fields", "danger", "growl-top-right");
-	return false;
+var isNotEmpty = function(value) {
+  if (value && value !== '') {
+    return true;
+  }
+  Bert.alert("Please fill in all fields", "danger", "growl-top-right");
+  return false;
 };
 
-// Check location fields
-isValidLocation = function(location){
-  //supposed to be checkign list of locations
-  //TODO:
-	if(location==asdf){
-		Bert.alert("location is not valid", "danger", "growl-top-right");
-		return false;
-	}
-	return true;
+// Check of locations in location fields exist
+// Warning: direct access to finalMap
+var isValidLocation = function(location) {
+  if (!finalMap[location]) {
+    Bert.alert("Location does not exist", "danger", "growl-top-right");
+    return false;
+  }
+  return true;
 };
--- NUShortcuts/client/js/navigation.js
+++ NUShortcuts/client/js/navigation.js
@@ -1,4 +1,4 @@
-Template.navigation.onRendered(function(){
+Template.navigation.onRendered(function() {
   $("#navigation-link").addClass('selected');
   $("#settings-link").removeClass('selected');
   $("#locationSearch-link").removeClass('selected');
@@ -6,73 +6,70 @@ Template.navigation.onRendered(function(){
   $("#map-link").removeClass('selected');
 });
 
-Template.navigation.onCreated = function(){
-
-};
-
-
 Template.navigation.events({
-  "submit .navigation": function(event){
+  "submit .navigation": function(event) {
     var startLocation = trimInput(event.target.startLocation.value);
     var endLocation = trimInput(event.target.endLocation.value);
 
-    if(isNotEmpty(startLocation) &&
-    isNotEmpty(endLocation) &&
-    isValidLocation(startLocation) &&
-    isValidLocation(endLocation) ){
+    if (isNotEmpty(startLocation) &&
+      isNotEmpty(endLocation) &&
+      isValidLocation(startLocation) &&
+      isValidLocation(endLocation)) {
 
-      Bert.alert("Calculating paths", "success", "growl-top-right");
+      Bert.alert("Calculations done", "success", "growl-top-right");
 
-      event.target.startLocation.value = startLocation;
-      event.target.endLocation.value =  endLocation;
+      //calls dijkstras algo and puts results in the 4 query templates
+      var timeResult = g.getQueryOutput(startLocation, endLocation, 1).split(" : ");
+      $('#timeTime').text("Time needed: " + timeResult[1]);
+      $('#timePath').text("Path: " + timeResult[0]);
+      var distanceResult = g.getQueryOutput(startLocation, endLocation, 2).split(" : ");
+      $('#distanceTime').text("Time needed: " + distanceResult[1]);
+      $('#distancePath').text("Path: " + distanceResult[0]);
+      var stairsResult = g.getQueryOutput(startLocation, endLocation, 3).split(" : ");
+      $('#stairsTime').text("Time needed:  " + stairsResult[1]);
+      $('#stairsPath').text("Path: " + stairsResult[0]);
+      var shelterResult = g.getQueryOutput(startLocation, endLocation, 4).split(" : ");
+      $('#shelterTime').text("Time needed: " + shelterResult[1]);
+      $('#shelterPath').text("Path: " + shelterResult[0]);
 
       //shows the query layout
       $(".queryLayout").css('visibility', 'visible');
 
-      //calls dijkstras algo and displays results in the 4 query templates
-      var timeResult = g.getQueryOutput(startLocation, endLocation, 1).split(" : ");
-      $('#timeTime').text("Time needed: " + timeResult[1] );
-      $('#timePath').text("Path: " + timeResult[0] );
-      var distanceResult = g.getQueryOutput(startLocation, endLocation, 2).split(" : ");
-      $('#distanceTime').text("Time needed: " + distanceResult[1] );
-      $('#distancePath').text("Path: " + distanceResult[0] );
-      var stairsResult = g.getQueryOutput(startLocation, endLocation, 3).split(" : ");
-      $('#stairsTime').text("Time needed:  " + stairsResult[1] );
-      $('#stairsPath').text("Path: " + stairsResult[0] );
-      var shelterResult = g.getQueryOutput(startLocation, endLocation, 4).split(" : ");
-      $('#shelterTime').text("Time needed: " + shelterResult[1] );
-      $('#shelterPath').text("Path: " + shelterResult[0] );
-
-      //TODO: link to maps page
-    }else {
+      //TODO: link to maps page for each query
+    } else {
       //hides the query layout
       $(".queryLayout").css('visibility', 'hidden');
     }
 
-    return false;//prevent submitting of form
+    //keeps the input inside the field
+    event.target.startLocation.value = startLocation;
+    event.target.endLocation.value = endLocation;
+
+    return false; //prevent submitting of form
   }
 });
 
 //Validation Rules
 
 // Trim Helper
-var trimInput = function(val){
-	return val.replace(/^\s*|\s*$/g, "");
+var trimInput = function(val) {
+  return val.replace(/^\s*|\s*$/g, "");
 };
 
-var isNotEmpty = function(value){
-	if (value && value !== ''){
-		return true;
-	}
-	Bert.alert("Please fill in all fields", "danger", "growl-top-right");
-	return false;
+var isNotEmpty = function(value) {
+  if (value && value !== '') {
+    return true;
+  }
+  Bert.alert("Please fill in all fields", "danger", "growl-top-right");
+  return false;
 };
 
 // Check of locations in location fields exist
-isValidLocation = function(location){
-	if(!g.vertexExists){
-		Bert.alert("location is not valid", "danger", "growl-top-right");
-		return false;
-	}
-	return true;
+// Warning: direct access to finalMap
+var isValidLocation = function(location) {
+  if (!finalMap[location]) {
+    Bert.alert("Location does not exist", "danger", "growl-top-right");
+    return false;
+  }
+  return true;
 };

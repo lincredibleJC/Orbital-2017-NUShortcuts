d83195a6954a1b104882ec6f65d3a4e791b42f13
Jiachao Lin <lin_jiachao@hotmail.com>
2017-06-25 13:36:35 +0800

    Add Reactive GoogleMaps
    No location markers in MongoDB yet, so nothing to interact with

--- NUShortcuts/client/js/map.js
+++ NUShortcuts/client/js/map.js
@@ -1,13 +1,92 @@
-Template.map.rendered = function(){
+Template.map.onRendered(function() {
   $("#map-link").addClass('selected');
   $("#navigation-link").removeClass('selected');
   $("#locationSearch-link").removeClass('selected');
   $("#popularLocations-link").removeClass('selected');
   $("#settings-link").removeClass('selected');
-}
+
+  //load google maps
+  GoogleMaps.load();
+});
+
+Template.map.onCreated(function() {
+  // We can use the `ready` callback to interact with the map API once the map is ready.
+  GoogleMaps.ready('nusMap', function(map) {
+
+    // Add a marker to the map once it's ready
+    var marker = new google.maps.Marker({
+      position: map.options.center,
+      map: map.instance
+    });
+
+    // /This will insert the coordinates into the markers collection where the map was clicked.
+    google.maps.event.addListener(map.instance, 'click', function(event) {
+     Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+   });
+
+   //declare empty set of markers
+   var markers = {};
+
+    Markers.find().observe({
+      added: function(document) {
+        // Create a marker for this document
+        var marker = new google.maps.Marker({
+          draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: new google.maps.LatLng(document.lat, document.lng),
+          map: map.instance,
+          // We store the document _id on the marker in order
+          // to update the document within the 'dragend' event below.
+          id: document._id
+        });
+
+        // This listener lets us drag markers on the map and update their corresponding document.
+        google.maps.event.addListener(marker, 'dragend', function(event) {
+          Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
+        });
+
+        // Store this marker instance within the markers object.
+        markers[document._id] = marker;
+      },
+      changed: function(newDocument, oldDocument) {
+        markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
+      },
+      removed: function(oldDocument) {
+        // Remove the marker from the map
+        markers[oldDocument._id].setMap(null);
+
+        // Clear the event listener
+        google.maps.event.clearInstanceListeners(
+          markers[oldDocument._id]);
+
+        // Remove the reference to this marker instance
+        delete markers[oldDocument._id];
+      }
+    });
+
+
+
+  });
+});
+
+Template.map.helpers({
+  exampleMapOptions: function() {
+    // Make sure the maps API has loaded
+    if (GoogleMaps.loaded()) {
+      // Map initialization options
+      return {
+        center: new google.maps.LatLng(1.2966, 103.7764),
+        zoom: 16
+      };
+    }
+  }
+});
 
 Template.map.events({
     'click': function(){
         console.log("You clicked something");
+        //access maps instance though maps object
+        GoogleMaps.maps.exampleMap.instance;
+
     }
 });

90afcbd5027bc07cd30c776127540dc183445e84
Jiachao Lin <lin_jiachao@hotmail.com>
2017-06-25 14:06:57 +0800

    Add Reactive geolocation, Remove Reactive maps
    code from
    http://meteorcapture.com/reactive-geolocation-with-google-maps/

--- NUShortcuts/client/js/map.js
+++ NUShortcuts/client/js/map.js
@@ -1,92 +1,81 @@
-Template.map.onRendered(function() {
-  $("#map-link").addClass('selected');
-  $("#navigation-link").removeClass('selected');
-  $("#locationSearch-link").removeClass('selected');
-  $("#popularLocations-link").removeClass('selected');
-  $("#settings-link").removeClass('selected');
+if (Meteor.isClient) {
+  var MAP_ZOOM = 16;
 
-  //load google maps
-  GoogleMaps.load();
-});
-
-Template.map.onCreated(function() {
-  // We can use the `ready` callback to interact with the map API once the map is ready.
-  GoogleMaps.ready('nusMap', function(map) {
-
-    // Add a marker to the map once it's ready
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
-
-    // /This will insert the coordinates into the markers collection where the map was clicked.
-    google.maps.event.addListener(map.instance, 'click', function(event) {
-     Markers.insert({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+  Meteor.startup(function() {
+    //load google maps
+     GoogleMaps.load();
    });
 
-   //declare empty set of markers
-   var markers = {};
-
-    Markers.find().observe({
-      added: function(document) {
-        // Create a marker for this document
-        var marker = new google.maps.Marker({
-          draggable: true,
-          animation: google.maps.Animation.DROP,
-          position: new google.maps.LatLng(document.lat, document.lng),
-          map: map.instance,
-          // We store the document _id on the marker in order
-          // to update the document within the 'dragend' event below.
-          id: document._id
-        });
-
-        // This listener lets us drag markers on the map and update their corresponding document.
-        google.maps.event.addListener(marker, 'dragend', function(event) {
-          Markers.update(marker.id, { $set: { lat: event.latLng.lat(), lng: event.latLng.lng() }});
-        });
-
-        // Store this marker instance within the markers object.
-        markers[document._id] = marker;
-      },
-      changed: function(newDocument, oldDocument) {
-        markers[newDocument._id].setPosition({ lat: newDocument.lat, lng: newDocument.lng });
-      },
-      removed: function(oldDocument) {
-        // Remove the marker from the map
-        markers[oldDocument._id].setMap(null);
-
-        // Clear the event listener
-        google.maps.event.clearInstanceListeners(
-          markers[oldDocument._id]);
-
-        // Remove the reference to this marker instance
-        delete markers[oldDocument._id];
-      }
-    });
-
-
-
+   //redner selected property on sidebar
+  Template.map.onRendered(function() {
+    $("#map-link").addClass('selected');
+    $("#navigation-link").removeClass('selected');
+    $("#locationSearch-link").removeClass('selected');
+    $("#popularLocations-link").removeClass('selected');
+    $("#settings-link").removeClass('selected');
   });
-});
 
-Template.map.helpers({
-  exampleMapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(1.2966, 103.7764),
-        zoom: 16
-      };
+  Template.map.onCreated(function() {
+    // We can use the `ready` callback to interact with the map API once the map is ready.
+    GoogleMaps.ready('nusMap', function(map) {
+      var marker;
+
+      // Create and move the marker when latLng changes.
+      self.autorun(function() {
+        var latLng = Geolocation.latLng();
+        if (! latLng)
+          return;
+
+        // If the marker doesn't yet exist, create it.
+        if (! marker) {
+          marker = new google.maps.Marker({
+          position: new google.maps.LatLng(latLng.lat, latLng.lng),
+          map: map.instance
+          });
+        }
+        // The marker already exists, so we'll just change its position.
+        else {
+          marker.setPosition(latLng);
+        }
+
+        // Center and zoom the map view onto the current position.
+        map.instance.setCenter(marker.getPosition());
+        map.instance.setZoom(MAP_ZOOM);
+      });
+    });
+  });
+
+
+  Template.map.helpers({
+    geolocationError: function() {
+      var error = Geolocation.error();
+      return error && error.message;
+    },
+    mapOptions: function() {
+      var latLng = Geolocation.latLng();
+      // Initialize the map once we have the latLng.
+      if (GoogleMaps.loaded() && latLng) {
+        return {
+          center: new google.maps.LatLng(latLng.lat, latLng.lng),
+          zoom: MAP_ZOOM
+        };
+      }else if (GoogleMaps.loaded()) {//if geolocation fails, Make sure the maps API has loaded
+        // Map initialization options
+        return {
+          center: new google.maps.LatLng(1.2966, 103.7764),//NUS LatLng
+          zoom: MAP_ZOOM
+        };
+      }
     }
-  }
-});
+  });
 
-Template.map.events({
-    'click': function(){
-        console.log("You clicked something");
-        //access maps instance though maps object
-        GoogleMaps.maps.exampleMap.instance;
+  Template.map.events({
+      'click': function(){
+          console.log("You clicked something");
+          //access maps instance though maps object
+          GoogleMaps.maps.exampleMap.instance;
 
-    }
-});
+      }
+  });
+
+}
